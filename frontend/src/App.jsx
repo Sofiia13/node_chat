5ce60861f3a947baa9ba7e916f5156f7a428cde8
Sofiia13 <sophiyastanish@gmail.com>
2026-02-03 19:40:08 +0200

    add basic components to the chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Login } from './pages/Login';
+import { useEffect } from 'react';
+import { Chat } from './pages/Chat';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isUser, setIsUser] = useState(false);
+  const [currentUsername, setCurrentUsername] = useState('');
+
+  useEffect(() => {
+    const checkAuth = () => {
+      const username = localStorage.getItem('username');
+      if (username) {
+        setIsUser(true);
+        setCurrentUsername(username);
+      } else {
+        setIsUser(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  const handleLogin = () => {
+    setIsUser(true);
+    const username = localStorage.getItem('username');
+    setCurrentUsername(username);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        {isUser ? (
+          <Chat currentUsername={currentUsername} />
+        ) : (
+          <Login onLoginSuccess={handleLogin} />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Chat/Chat.jsx
@@ -0,0 +1,21 @@
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { Message } from '../../components/Message';
+import styles from './Chat.module.scss';
+
+export const Chat = ({ currentUsername }) => {
+  return (
+    <div className={styles.chat}>
+      <h1>Chat Page</h1>
+      <div className={styles.chat__inputGroup}>
+        <Input
+          placeholder="Input your message..."
+          value={''}
+          onChange={() => {}}
+        />
+        <Button onClick={() => {}}>Send</Button>
+      </div>
+      <Message text="Hello, how are you?" sender={currentUsername} />
+    </div>
+  );
+};
